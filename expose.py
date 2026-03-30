b30c218035dfa9c5a45bb5da4c82bc032be439e2
Harish Raj <swarabala@gmail.com>
2026-03-30 17:16:04 +0530

    Replace ngrok auth token with placeholder
    Updated ngrok authentication token placeholder.

--- expose.py
+++ expose.py
@@ -2,7 +2,7 @@ from pyngrok import ngrok
 import time
 
 # Set your authtoken from ngrok.com (free)
-ngrok.set_auth_token("3Bf5tFICys1Jd29KtSiej6gpqoE_5R3PEcdZCetc8SGEgbAMU")
+ngrok.set_auth_token("YOUR_TOKEN")
 
 # Create tunnel to port 8000
 public_url = ngrok.connect(8000)
